b3f5960a2a37b739e82daf104621cb244350349c
Wei <wei.cao@pinkoi.com>
2024-10-01 18:07:11 +0800

    [Chore] test for Nuke

--- test-dependabot-package/MyPackage-1/Package.swift
+++ test-dependabot-package/MyPackage-1/Package.swift
@@ -7,5 +7,6 @@ let package = Package(
     name: "MyPackage-11-1",
     dependencies: [
         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "11.3.0"),
+        .package(url: "https://github.com/kean/Nuke", exact: "12.5.0"),
     ]
 )
--- test-dependabot-package/MyPackage-2/Package.swift
+++ test-dependabot-package/MyPackage-2/Package.swift
@@ -7,5 +7,6 @@ let package = Package(
     name: "MyPackage-11-2",
     dependencies: [
         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "11.2.0"),
+        .package(url: "https://github.com/kean/Nuke", exact: "12.6.0"),
     ]
 )
