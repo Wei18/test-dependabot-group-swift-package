b6279206e53e889a2d170212809fce767b9d486c
Wei <wei.cao@pinkoi.com>
2024-10-01 14:39:31 +0800

    [Test] for firebase-ios-sdk

---
+++ test-dependabot-package/MyPackage-2/Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MyPackage-2",
+    dependencies: [
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "11.3.0"),
+    ]
+)
